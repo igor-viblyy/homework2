0095b0693181bca3d71be47b8653a9a6f7180831
Igor Viblyy <igor.viblyy@gmail.com>
2017-06-12 01:08:49 +0300

    add abject_model

--- lib/topics/object_model.rb
+++ lib/topics/object_model.rb
@@ -2,4 +2,26 @@
 
 # exercise object model
 module ObjectModel
+  # module LinuxFriendly
+  module LinuxFriendly
+    def fork_process
+      super
+    end
+  end
+
+  # class Computer it`s a parent class
+  class Computer
+    def mine_bitcoins
+      inspect
+    end
+
+    def fork_process
+      'Parent: allocate memory'
+    end
+  end
+
+  # class Laptop it`s a child class
+  class Laptop < Computer
+    prepend LinuxFriendly
+  end
 end
